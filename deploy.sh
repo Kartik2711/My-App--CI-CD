6820d6cbd19c735328a17f169b14d80ab5ef07fc
Kartik2711 <kartik731051@gmail.com>
2026-04-14 16:02:22 +0530

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-set -e  # stop on error
+set -e
 
-cd /home/ec2-user/my-git-project
-
-echo "📥 Pull latest code"
-git pull origin main
-
-echo "🐍 Setup virtual environment"
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-fi
-
-source venv/bin/activate
+cd /home/ec2-user/myapp   # MUST match appspec.yml
 
 echo "📦 Install dependencies"
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "🛑 Stop old app"
 pkill -f app.py || true

97bef239530527697dace00898a83990e50fe73f
Kartik2711 <kartik731051@gmail.com>
2026-04-15 12:29:01 +0530

    Test CI/CD pipeline

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello test 🚀 Git Project Working!"
+    return "Hello kartik 🚀 Git Project Working!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
